b73d15229a627e8a01c7c92442cc0ff1e2162814
Vedmaka <god.vedmaka@gmail.com>
2016-01-04 00:14:54 +0300

    rr

--- extensions/authorRating/modules/ext.authorRating.js
+++
@@ -1,7 +0,0 @@
-( function () {
-
-    if( $('.author-rating-wrapper').length ) {
-        var ar = new mw.Authorrating($('.author-rating-wrapper'));
-    }
-
-}() );
--- extensions/authorRating/schema/author_rating.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE /*_*/author_rating (
-  user_id int DEFAULT NULL,
-  author_user_id int DEFAULT NULL,
-  created_at int(11) DEFAULT NULL,
-  page_id int DEFAULT NULL
-)/* $wgDbTableOptions */;
