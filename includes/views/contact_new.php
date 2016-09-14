f87a9d9de34cd84b515524f6c7a740f228858f11
Vedmaka <god.vedmaka@gmail.com>
2016-09-15 00:26:53 +0300

    + styles for contact_us form

--- includes/views/contact_new.php
+++ includes/views/contact_new.php
@@ -21,12 +21,12 @@
 <div id="contactus-form">
 
     <div class="row">
-        <div class="col-sm-10 col-lg-offset-1">
+        <div class="col-sm-10 col-sm-offset-1">
             <form method="post" class="form-horizontal">
 
                 <div class="form-group">
                     <label for="input-why" class="col-sm-2"><?=wfMessage('settlein-skin-project-page-contact-field-reason')->plain()?></label>
-                    <div class="col-sm-10">
+                    <div class="col-md-9 col-lg-8">
                         <select name="reason" id="input-why" class="form-control" required>
                             <option value="0" selected><?=wfMessage('settlein-skin-project-page-contact-field-reason-value-1')->plain()?></option>
                             <option value="1"><?=wfMessage('settlein-skin-project-page-contact-field-reason-value-2')->plain()?></option>
@@ -37,21 +37,21 @@
 
                 <div class="form-group">
                     <label for="input-name" class="col-sm-2"><?=wfMessage('settlein-skin-project-page-contact-field-name')->plain()?> <span class="text-danger">*</span> </label>
-                    <div class="col-sm-10">
+                    <div class="col-md-9 col-lg-8">
                         <input type="text" class="form-control" placeholder="" name="name" id="input-name" required>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="input-email" class="col-sm-2"><?=wfMessage('settlein-skin-project-page-contact-field-email')->plain()?> <span class="text-danger">*</span> </label>
-                    <div class="col-sm-10">
+                    <div class="col-md-9 col-lg-8">
                         <input type="email" name="email" class="form-control" placeholder="" id="input-email" required>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <label for="input-message" class="col-sm-2"><?=wfMessage('settlein-skin-project-page-contact-field-message')->plain()?> <span class="text-danger">*</span></label>
-                    <div class="col-sm-10">
+                    <div class="col-md-9 col-lg-8">
                         <textarea class="form-control" rows="4" id="input-message" name="message" required></textarea>
                     </div>
                 </div>
