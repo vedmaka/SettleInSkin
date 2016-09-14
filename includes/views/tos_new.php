f305c4b6771bb5511d15764640f50b60c38fb97e
Vedmaka <god.vedmaka@gmail.com>
2016-09-15 01:00:19 +0300

    + same height for TOS

--- includes/views/tos_new.php
+++ includes/views/tos_new.php
@@ -1,12 +1,16 @@
 <div class="container tos-about-page-container">
 
-    <div class="about-us-section-heading">
-	    <div class="about-us-header-wrapper">
-		    <span><?=wfMessage('settlein-skin-project-page-tos-title')->plain()?></span>
-	    </div>
-	    <div class="about-us-header-text-wrapper">
-			<?=wfMessage('settlein-skin-project-page-tos-title-text')->plain()?>
-	    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="about-us-section-heading">
+                <div class="about-us-header-wrapper">
+                    <span><?=wfMessage('settlein-skin-project-page-tos-title')->plain()?></span>
+                </div>
+                <div class="about-us-header-text-wrapper">
+                    <?=wfMessage('settlein-skin-project-page-tos-title-text')->plain()?>
+                </div>
+            </div>
+        </div>
     </div>
 
 	<section class="tos-about-items-wrapper">
@@ -27,3 +31,19 @@
 	</section>
 
 </div>
+
+<script>
+    /* Make boxes have same height - not so elegant solution though */
+    $(function() {
+
+        function fixBlockHeight() {
+            var height = $('.tos-item-short > ol').height() + 40;
+            $('#tos-height-scroll').css('height', height + 'px');
+        }
+
+        setTimeout( fixBlockHeight, 0 );
+
+        $(window).on('resize', fixBlockHeight);
+
+    });
+</script>
