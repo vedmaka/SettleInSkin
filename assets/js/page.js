569fb8ec96bb134cadcff8531d58f791fbdda057
vedmaka <god.vedmaka@gmail.com>
2017-04-10 03:14:24 +0300

    error box

--- assets/js/page.js
+++ assets/js/page.js
@@ -74,4 +74,36 @@ $(function () {
 		}
 	});
 
+    //TODO: remove duplications
+
+    var textInput = $(this).find('.selectize-search-appendix');
+    var geoInput = $(this).find('.settle-geo-search-input');
+    var wrapper = $(this).find('.settle-geo-search-wrapper');
+
+    $('#searchform_smw').submit(function(event){
+
+        if( wrapper.find('.settle-geo-search-input-error').length ) {
+            wrapper.find('.settle-geo-search-input-error').remove();
+        }
+
+        if( !geoInput.val() ) {
+            var error = $('<div/>');
+            error.html('Oops, please specify a location!');
+            error.addClass('settle-geo-search-input-error');
+            wrapper.append(error);
+            geoInput.get(0).selectize.focus();
+            event.preventDefault();
+            return false;
+        }
+
+    });
+
+    geoInput.on('change', function(){
+        if( $(this).val() ) {
+            if( wrapper.find('.settle-geo-search-input-error').length ) {
+                wrapper.find('.settle-geo-search-input-error').remove();
+            }
+        }
+    });
+
 });
